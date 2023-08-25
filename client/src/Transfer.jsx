9aeadbac71334908951f84ba83682694d02f4b3a
Ericson Cabral <tncl@gft.com>
2023-08-25 14:45:57 -0300

    Task done

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,23 +1,57 @@
 import { useState } from "react";
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import {secp256k1} from 'ethereum-cryptography/secp256k1';
+import { toHex,utf8ToBytes } from 'ethereum-cryptography/utils';
+import { keccak256 } from "ethereum-cryptography/keccak";
 
-function Transfer({ address, setBalance }) {
+// Private key: 34ad93b68c53a7573070df6dbc1c2d577c5d580feaad56ac65f6d86b109948a2
+// Public key: 0336a290581f9ae77fe122bcbb374d5b6ec4a7bb15fef3b095de95c238760960b3
+
+// Private key: 7c3c91362a14d3e9dbcda1d4e6b901ca2a7a1e59bfcf8d40fded44d4a48ef75e
+// Public key: 02ddd3bcca8dd986d2f6cccb471e4a83a5fbaaa9da9c5022d88231b30ba19cc4b4
+
+// Private key: d189b69c0144c8e92c96694a006110ad277cafe319c3fad604159b6df0bd2d59
+// Public key: 02790a03aa3393b37a8d17cd9e3368a88f1bdcd526e12c46efdbd12df2f8eb8bb6
+
+function Transfer({ address, setBalance, privateKey }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
+  const hashMessage = message => keccak256(Uint8Array.from(message));
+  const signMessage = msg => secp256k1.sign(hashMessage(msg), privateKey);
+
   async function transfer(evt) {
     evt.preventDefault();
+
+    const msg = { amount: parseInt(sendAmount), recipient };
+    const sig = signMessage(msg);
+
+    const stringifyBigInts = obj =>{
+      for(let prop in obj){
+        let value = obj[prop];
+        if(typeof value === 'bigint'){
+          obj[prop] = value.toString();
+        }else if(typeof value === 'object' && value !== null){
+          obj[prop] = stringifyBigInts(value);
+        }
+      }
+      return obj;
+    }
+
+    // stringify bigints before sending to server
+    const sigStringed = stringifyBigInts(sig);
+  
+    const tx = {
+      sig:sigStringed, msg, sender: address
+    }
 
     try {
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = await server.post(`send`, tx);
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require('ethereum-cryptography/secp256k1');
+const {keccak256} = require('ethereum-cryptography/keccak');
 
 app.use(cors());
 app.use(express.json());
 
+// public keys
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0336a290581f9ae77fe122bcbb374d5b6ec4a7bb15fef3b095de95c238760960b3": 100,
+  "02ddd3bcca8dd986d2f6cccb471e4a83a5fbaaa9da9c5022d88231b30ba19cc4b4": 50,
+  "02790a03aa3393b37a8d17cd9e3368a88f1bdcd526e12c46efdbd12df2f8eb8bb6": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +22,24 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+
+  // TODO: get a signature from the client-side application
+  // recover the public address from the signature
+
+  const { sender, sig:sigStringed, msg } = req.body;
+  const { recipient, amount } = msg;
+
+  const sig = {
+    ...sigStringed,
+    r: BigInt(sigStringed.r),
+    s: BigInt(sigStringed.s)
+  }
+
+  const hashMessage = (message) => keccak256(Uint8Array.from(message));
+
+  const isValid = secp.secp256k1.verify(sig, hashMessage(msg), sender) === true;
+
+  if(!isValid) res.status(400).send({ message: "Bad signature!" });
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
